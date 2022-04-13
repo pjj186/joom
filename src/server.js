e6fd050fd2e78252e1420a72c1e84024c58c0357
Jong jae Park <pjj186@naver.com>
2022-04-14 01:21:57 +0900

    pug setup

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-console.log("Hello");
+app.set("view engine", "pug"); // 템플릿 엔진 설정
+app.set("views", __dirname + "/src/views"); // 템플릿들이 모여있는 곳 가르키기
 
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
 app.listen(PORT, handleListen);
