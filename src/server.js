ebc87fc9c7eae2ad5533367d05086a83677b1b34
Jong jae Park <pjj186@naver.com>
2022-04-14 01:44:23 +0900

    Pug 설명 + MVP CSS 설정
    Pug 템플릿 엔진에 대한 간단한 설명과 MVP CSS를 설정했습니다.

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const PORT = 4000;
 const app = express();
 
 app.set("view engine", "pug"); // 템플릿 엔진 설정
-app.set("views", __dirname + " /views"); // 템플릿들이 모여있는 곳 가르키기
+app.set("views", __dirname + "/views"); // 템플릿들이 모여있는 곳 가르키기
 app.use("/public", express.static(__dirname + "/public")); // Express 정적 파일 제공
 // __dirname : 현재 이 파일이 있는 폴더 경로
 
