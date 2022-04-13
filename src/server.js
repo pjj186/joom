2c9724e58a52970417907bbe908172457cd8a087
Jong jae Park <pjj186@naver.com>
2022-04-14 01:35:19 +0900

    express static
    /public 경로로 접근 시 정적 파일에 접근할 수 있도록 함

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const PORT = 4000;
 const app = express();
 
 app.set("view engine", "pug"); // 템플릿 엔진 설정
-app.set("views", __dirname + "/src/views"); // 템플릿들이 모여있는 곳 가르키기
+app.set("views", __dirname + " /views"); // 템플릿들이 모여있는 곳 가르키기
+app.use("/public", express.static(__dirname + "/public")); // Express 정적 파일 제공
+// __dirname : 현재 이 파일이 있는 폴더 경로
+
+app.get("/", (req, res) => res.render("home")); // home.pug 렌더링
 
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
 app.listen(PORT, handleListen);
