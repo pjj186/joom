decd3fd6bd05abb788820a68c38e7f684ca5aaed
Jong jae Park <pjj186@naver.com>
2022-04-19 02:18:25 +0900

    메시지 처리

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,15 +1,23 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`); // 프론트엔드단의 소켓 서버의 "/" Route로 소켓을 보내고있음
 
 // 여기서 socket.send() 를 사용하면 백엔드 쪽으로 값을 보냄
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New message : ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -19,8 +27,15 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
