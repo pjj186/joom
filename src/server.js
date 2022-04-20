ab972070e142e1c0bf07a7e3b9e09515f8483c43
Jong jae Park <pjj186@naver.com>
2022-04-21 01:56:54 +0900

    SocketIO 연습

--- src/server.js
+++ src/server.js
@@ -19,7 +19,12 @@ const httpServer = http.createServer(app); // http 서버 생성
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  console.log(socket);
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
 });
 
 // const sockets = [];
