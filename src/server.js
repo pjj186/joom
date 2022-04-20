40393ad2db65a6f20473e9a19273f0921a77e4db
Jong jae Park <pjj186@naver.com>
2022-04-21 01:11:15 +0900

    SocketIO 설치

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
-import { WebSocketServer } from "ws";
 import express from "express";
-import { parse } from "path";
+import SocketIO from "socket.io";
 
 const PORT = 4000;
 const app = express();
@@ -16,37 +15,38 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
 
-const server = http.createServer(app); // http 서버 생성
-const wss = new WebSocketServer({
-  server,
-}); // websocket 서버 생성 (http 서버 위에)
+const httpServer = http.createServer(app); // http 서버 생성
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  // 프론트쪽에서 보내는 socket
-  // 여기서 socket.send() 를 사용하면 프론트쪽으로 값을 보냄
-  sockets.push(socket); // 브라우저와 연결 될 때 마다 소켓하나씩 추가 (각 브라우저에서 socket 객체를 만드니깐)
-  socket["nickname"] = "Anon";
-  console.log("Connected to Browser ✅");
-  socket.on("close", () => {
-    console.log("Disconnected from Browser ❌");
-  });
-  socket.on("message", (msg) => {
-    // 프론트단에서 넘어오는 메시지를 처리하는 곳
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload; // socket 객체에 nickname 프로퍼티를 payload와 함께 추가하는것 (대괄호 표기법)
-        console.log(socket);
-        break;
-    }
-  });
+wsServer.on("connection", (socket) => {
+  console.log(socket);
 });
 
-server.listen(PORT, handleListen);
+// const sockets = [];
+// wss.on("connection", (socket) => {
+//   // 프론트쪽에서 보내는 socket
+//   // 여기서 socket.send() 를 사용하면 프론트쪽으로 값을 보냄
+//   sockets.push(socket); // 브라우저와 연결 될 때 마다 소켓하나씩 추가 (각 브라우저에서 socket 객체를 만드니깐)
+//   socket["nickname"] = "Anon";
+//   console.log("Connected to Browser ✅");
+//   socket.on("close", () => {
+//     console.log("Disconnected from Browser ❌");
+//   });
+//   socket.on("message", (msg) => {
+//     // 프론트단에서 넘어오는 메시지를 처리하는 곳
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`)
+//         );
+//         break;
+//       case "nickname":
+//         socket["nickname"] = message.payload; // socket 객체에 nickname 프로퍼티를 payload와 함께 추가하는것 (대괄호 표기법)
+//         console.log(socket);
+//         break;
+//     }
+//   });
+// });
+
+httpServer.listen(PORT, handleListen);
