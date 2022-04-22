aca2e1a84e9383136796661b7b4cfdc3ff8fcac8
Jong jae Park <pjj186@naver.com>
2022-04-22 17:29:45 +0900

    Camera Switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -17,13 +17,18 @@ let muted = false;
 let cameraOff = false;
 
 const getCameras = async () => {
+  // 현재 컴퓨터에 연결된 카메라 장치 select에 추가해주는 함수
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected;
+      }
       cameraSelect.appendChild(option);
     });
   } catch (e) {
@@ -31,14 +36,24 @@ const getCameras = async () => {
   }
 };
 
-const getMedia = async () => {
+const getMedia = async (deviceId) => {
+  const initialConstrains = {
+    // deviceId가 없을때 실행, cameras를 만들기 전
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
     myFace.srcObject = myStream;
-    await getCameras();
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -114,10 +129,15 @@ const handleCameraClick = () => {
   }
 };
 
+const handleCameraChange = async () => {
+  await getMedia(cameraSelect.value);
+};
+
 form.addEventListener("submit", handleRoomSubmit);
 nameForm.addEventListener("submit", handleNicknameSubmit);
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+cameraSelect.addEventListener("input", handleCameraChange);
 
 socket.on("welcome", (user, newCount) => {
   const h3 = room.querySelector("h3");
