22dcf8017ecd25356ff65b98a7cf695bfda485f1
Jong jae Park <pjj186@naver.com>
2022-04-23 00:07:57 +0900

    rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,20 +1,17 @@
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("#enter");
-const room = document.getElementById("room");
-const nameForm = document.querySelector("#name");
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const cameraSelect = document.getElementById("cameras");
+const call = document.getElementById("call");
 
-room.hidden = true;
+call.hidden = true;
 
-let roomName;
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
 
 const getCameras = async () => {
   // 현재 컴퓨터에 연결된 카메라 장치 select에 추가해주는 함수
@@ -59,50 +56,6 @@ const getMedia = async (deviceId) => {
   }
 };
 
-const addMessage = (message) => {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message;
-  ul.appendChild(li);
-};
-
-const handleMessageSubmit = (event) => {
-  event.preventDefault();
-  const input = room.querySelector("#msg input");
-  const value = input.value;
-  socket.emit("new_message", input.value, roomName, () => {
-    addMessage(`You : ${value}`);
-  });
-
-  input.value = "";
-};
-
-const handleNicknameSubmit = (event) => {
-  event.preventDefault();
-  const input = welcome.querySelector("#name input");
-  socket.emit("nickname", input.value);
-  input.value = "";
-};
-
-const showRoom = (newCount) => {
-  welcome.hidden = true;
-  room.hidden = false;
-  getMedia();
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  const msgForm = room.querySelector("#msg");
-  msgForm.addEventListener("submit", handleMessageSubmit);
-};
-
-// 방 입장
-const handleRoomSubmit = (event) => {
-  event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom); // event submit
-  roomName = input.value;
-  input.value = "";
-};
-
 const handleMuteClick = () => {
   myStream
     .getAudioTracks()
@@ -133,37 +86,33 @@ const handleCameraChange = async () => {
   await getMedia(cameraSelect.value);
 };
 
-form.addEventListener("submit", handleRoomSubmit);
-nameForm.addEventListener("submit", handleNicknameSubmit);
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 cameraSelect.addEventListener("input", handleCameraChange);
 
-socket.on("welcome", (user, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${user} Joined!`);
-});
+// WelcomeForm
 
-socket.on("bye", (user, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${user} left!`);
-});
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
 
-socket.on("new_message", (msg) => {
-  addMessage(msg);
-});
+const startMedia = () => {
+  welcome.hidden = true;
+  call.hidden = false;
+  getMedia();
+};
 
-socket.on("room_change", (rooms) => {
-  const roomList = welcome.querySelector("ul");
-  roomList.innerHTML = "";
-  if (rooms.length === 0) {
-    return;
-  }
-  rooms.forEach((room) => {
-    const li = document.createElement("li");
-    li.innerText = room;
-    roomList.append(li);
-  });
+const handleWelcomeSubmit = (event) => {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  socket.emit("join_room", input.value, startMedia);
+  roomName = input.value;
+  input.value = "";
+};
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// Socket Code
+
+socket.on("welcome", () => {
+  console.log("Somebody Joined");
 });
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
 import express from "express";
-import { instrument } from "@socket.io/admin-ui";
-import { Server } from "socket.io";
+import SocketIO from "socket.io";
 
 const PORT = 4000;
 const app = express();
@@ -17,69 +16,15 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
 
 const httpServer = http.createServer(app); // http 서버 생성
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
 
-instrument(wsServer, {
-  auth: false,
-});
-
-const publicRooms = () => {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = wsServer; // sids : socket id들만 / rooms:  socket id, room 전부
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-};
-
-const countRoom = (roomName) => {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-};
+const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  wsServer.sockets.emit("room_change", publicRooms());
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    console.log(`Socket Event:${event}`);
-  });
-
-  socket.on("enter_room", (roomName, done) => {
+  socket.on("join_room", (roomName, done) => {
     socket.join(roomName);
-    done(countRoom(roomName)); // 실행 위치 : 프론트엔드
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // 나를 제외한 다른 사람들이 봄
-    wsServer.sockets.emit("room_change", publicRooms());
+    done();
+    socket.to(roomName).emit("welcome");
   });
-
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => {
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);
-    });
-  });
-
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done(); // 실행 위치 : 프론트엔드
-  });
-
-  socket.on("nickname", (nickName) => {
-    console.log(nickName);
-    socket["nickname"] = nickName;
-  }); // 대괄호 표기법으로 객체에 프로퍼티 추가
 });
 
 httpServer.listen(PORT, handleListen);
