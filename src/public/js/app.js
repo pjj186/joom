936acbbd70ec593405cf49efb10144c166afa177
Jong jae Park <pjj186@naver.com>
2022-04-23 18:24:23 +0900

    IceCandidate

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -125,20 +125,41 @@ socket.on("welcome", async () => {
 });
 
 socket.on("offer", async (offer) => {
+  console.log("received the offer");
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
   console.log(answer);
   myPeerConnection.setLocalDescription(answer);
   socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
 });
 
 socket.on("answer", (answer) => {
+  console.log("received the answer");
   myPeerConnection.setRemoteDescription(answer);
 });
 
+socket.on("ice", (ice) => {
+  console.log("reeceived candidate");
+  myPeerConnection.addIceCandidate(ice);
+});
+
 // RTC Code
+
+const handleIce = (data) => {
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
+};
+
+const handleAddStream = (data) => {
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.stream;
+};
+
 const makeConnection = () => {
-  const myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
   myStream
     .getTracks()
     .forEach((track) => myPeerConnection.addTrack(track, myStream));
